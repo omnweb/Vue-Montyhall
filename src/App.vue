1f608c91aee9f4ff055d8ac8bd594b66a4521407
netomatiazi <netoomn@gmail.com>
2020-09-10 14:05:25 -0300

    Upload logic archives

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,27 @@
 <template>
   <div id="app-2">
     <h1>Desafio de Montyhall</h1>  
-    <Door number="1" :hasGift="false" />  
+    <div class="form">
+      <div v-if="!started">
+        <!-- Só vai aparecer aqui se não estiver estartado -->
+        <label for="portsAmount">Selecione o número de portas:</label>
+        <input type="text" id="portsAmount" size="3" 
+          v-model.number="portsAmount">
+      </div>
+      <div v-if="!started">
+        <!-- Só vai aparecer aqui se não estiver estartado -->
+        <label for="selectedPort">Selecione o número da porta que contém o prêmio:</label>
+        <input type="text" id="selectedPort" size="3" 
+          v-model.number="selectedPort">
+      </div>
+      <button v-if="!started" @click="started= true" type="Button">Iniciar</button>
+      <button v-if="started" @click="started= false" type="Button">Reiniciar</button>
+    </div>
+    <div class="doors" v-if="started">
+      <div v-for="i in portsAmount" :key="i">
+        <Door :hasGift="i == selectedPort" :number="i"/>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,7 +29,16 @@
 import Door from './components/Door'
 export default {
   name:'App', 
-    components: {Door}
+    components: {Door},
+    data: function(){
+      //Estes dados representam o estado do componente
+      return{
+        started: false,
+        portsAmount: 3, 
+        selectedPort: null
+      }
+
+    }
 }
 </script>
 
@@ -48,25 +77,42 @@ body{
 .gift-top{
   width: 100px;
   height: 25px;
-  background-color: #36979c;
+  background-color: #bb8e35;
 }
 .gift-body {
   width: 90px;
   height: 60px;
-  background-color: #40bbc1;
+  background-color: #fabd45;
 }
 .gift-tie-1 {
   position: absolute;
   margin-top: 25px;
   width: 15px;
   height: 60px;
-  background-color: #fabd45;
+  background-color: red;
 }
 .gift-tie-2 { 
   position: absolute;
   margin-top: 45px;
   width: 90px;
   height: 15px;
-  background-color: #fabd45;
+  background-color: red;
+}
+.form {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 40px;
+}
+.form , .form input, .form button {
+  margin-bottom: 10px;
+  font-size: 2rem;
+}
+.doors{
+  display: flex;
+  align-self: stretch;
+  justify-content: space-around;
+  flex-wrap: wrap;
 }
 </style>
